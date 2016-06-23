a6348cb282f16e47be6ce9511fd59e5547601371
Pratik <pratsj9@live.in>
2016-06-23 11:40:23 +0530

    Added a few Comments

--- breaktime.py
+++ breaktime.py
@@ -1,3 +1,5 @@
+
+#This is a simple python program that opens the webbrowser with the specified URL after certain amount of Time breaks
 import webbrowser
 import time
 
@@ -6,7 +8,7 @@ break_counter = 0
 print("Current time is "+time.ctime())
 
 while i < 2:
-    time.sleep(5)
+    time.sleep(5) #wait 5 seconds till the next browser opening
     print("Looping",i)
     webbrowser.open("google.com")
     break_counter+=1
